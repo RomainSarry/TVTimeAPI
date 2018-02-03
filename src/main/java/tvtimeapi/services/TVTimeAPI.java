516cc8e76a915e9f93fe8f12d91fdd085b44d65f
Waale <romain.sarry@gmail.com>
2018-02-03 19:41:07 +0100

    Added log

--- src/main/java/tvtimeapi/services/TVTimeAPI.java
+++ src/main/java/tvtimeapi/services/TVTimeAPI.java
@@ -6,17 +6,21 @@ import tvtimeapi.beans.TVTimePage;
 import tvtimeapi.beans.TVTimeShow;
 import tvtimeapi.beans.TVTimeWatchlist;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Created by Romain on 01/11/2017.
  */
 public class TVTimeAPI {
+    private static final Logger LOGGER = Logger.getLogger(TVTimeAPI.class.getName());
+
     public static final String URL_BASE = "https://www.tvtime.com/";
 
     public static TVTimePage getPageByUrl(String url, String tvstRemember) {
         Document htmlDom = null;
 
         try {
-            System.out.println("Fetching URL : " + url);
             htmlDom = Jsoup.connect(url).cookie("tvstRemember", tvstRemember).get();
         }
         catch (Exception e) {
@@ -27,10 +31,12 @@ public class TVTimeAPI {
     }
 
     public TVTimeWatchlist getWatchlist(String tvstRemember) {
+        LOGGER.log(Level.INFO, "Fetching TVTime watchlist : " + URL_BASE);
         return new TVTimeWatchlist(getPageByUrl(URL_BASE, tvstRemember));
     }
 
     public TVTimeShow getShow(String id, String name, String episode, Integer remainingEpisodes, String poster, String link, String tvstRemember) {
+        LOGGER.log(Level.INFO, "Fetching TVShow \"" + name + "\" : " + URL_BASE + link);
         return new TVTimeShow(id, name, episode, remainingEpisodes, poster, link, tvstRemember);
     }
 }
